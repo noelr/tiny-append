08b0bccdc687505535a62f05009fe04dd78c6a68
Noel Rohrbach <nr@oxon.ch>
2016-12-29 13:37:19 +0100

    strings bite

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,8 +5,8 @@ module Lib (go) where
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import System.FilePath ((</>))
-import Prelude
 import Control.Monad.IO.Class
+import Data.Text.IO as TIO
 
 db = "log" </> "log"
 
@@ -18,5 +18,5 @@ go = scotty 4002 $ do
     file db
   post "/append" $ do
     l <- param "line"
-    liftIO $ appendFile db (mconcat [l, "\n"])
+    liftIO $ TIO.appendFile db (mconcat [l, "\n"])
     text "OK"
