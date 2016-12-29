144bb43fb857790d1fd36af61d83105600043626
Noel Rohrbach <nr@oxon.ch>
2016-12-29 11:50:37 +0100

    remove cors

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,7 +4,6 @@ module Lib (go) where
 
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import Network.Wai.Middleware.Cors
 import System.FilePath ((</>))
 import Prelude
 import Control.Monad.IO.Class
@@ -13,7 +12,6 @@ db = "log" </> "log"
 
 go :: IO ()
 go = scotty 4002 $ do
-  middleware simpleCors
   middleware logStdoutDev
   get "/" $ file db
   post "/append" $ do
